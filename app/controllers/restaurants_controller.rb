736d81f4b960d6b4b15f86fdbd16b90f1d5e7534
Thomas Sam-Yin-Yang <thomas.samyinyang@gmail.com>
2021-05-12 16:19:29 -0400

    Added the controllers, and the views.

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,17 +1,33 @@
 class RestaurantsController < ApplicationController
-  def index
+  before_action :set_restaurant, only: :show
 
+  def index
+    @restaurants = Restaurant.all
   end
 
   def new
-
+    @restaurant = Restaurant.new
   end
 
   def create
-
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to @restaurant
+    else
+      render :new
+    end
   end
 
   def show
+  end
 
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
   end
 end
